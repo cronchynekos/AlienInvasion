051a099ad7c406a98571ad0deefce4716db1c318
Kersplato <49539722+Kersplato@users.noreply.github.com>
2022-03-04 19:46:13 -0800

    landing page

--- landing_page.py
+++ landing_page.py
@@ -9,25 +9,28 @@ GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREY = (130, 130, 130)
+RED = (255,0,0)
+YELLOW = (255,255,0)
 
 
 class LandingPage:
-    alien_one_imgs = [pg.image.load(f'images/alien1.gif')]
-    alien_two_imgs = [pg.image.load(f'images/alien2.gif')]
-    alien_three_imgs = [pg.image.load(f'images/alien3.gif')]
-    ufo_imgs = [pg.image.load(f'images/alien4.gif')]
+    alien_one_imgs = [pg.image.load(f'images/alien6.gif')]
+    alien_two_imgs = [pg.image.load(f'images/alien7.gif')]
+    alien_three_imgs = [pg.image.load(f'images/alien8.gif')]
+    ufo_imgs = [pg.image.load(f'images/alien9.gif')]
 
     def __init__(self, game):
         self.screen = game.screen
         self.landing_page_finished = False
+        self.bg_img = pg.image.load(f'images/space2.png')
 
         headingFont = pg.font.SysFont(None, 192)
         subheadingFont = pg.font.SysFont(None, 122)
         font = pg.font.SysFont(None, 48)
 
-        strings = [('SPACE', WHITE, headingFont), ('INVADERS', GREEN, subheadingFont),
-                ('= 10 PTS', GREY, font), ('= 20 PTS', GREY, font),
-                            ('= 40 PTS', GREY, font), ('= ???', GREY, font),
+        strings = [('SPACE', WHITE, headingFont), ('INVADERS', RED, subheadingFont),
+                ('= 10 PTS', YELLOW, font), ('= 20 PTS', YELLOW, font),
+                            ('= 40 PTS', YELLOW, font), ('= ???', YELLOW, font),
                # ('PLAY GAME', GREEN, font), 
                 ('HIGH SCORES', GREY, font)]
 
@@ -42,18 +45,18 @@ class LandingPage:
 
         centerx = self.screen.get_rect().centerx
 
-        self.play_button = Button(self.screen, "PLAY GAME", ul=(centerx - 150, 650))
+        self.play_button = Button(self.screen, "PLAY GAME", ul=(centerx - 120, 650))
 
         n = len(self.texts)
         self.rects = [self.get_text_rect(text=self.texts[i], centerx=centerx, centery=self.posns[i]) for i in range(n)]
         self.alien_one = Alien(game=game, image_list=LandingPage.alien_one_imgs, 
-                               v=Vector(), ul=(centerx - 120, 385))
+                               v=Vector(), ul=(centerx - 170, 350))
         self.alien_two = Alien(game=game, image_list=LandingPage.alien_two_imgs, 
-                               v=Vector(), ul=(centerx - 180, 445))
+                               v=Vector(), ul=(centerx - 180, 420))
         self.alien_three = Alien(game=game, image_list=LandingPage.alien_three_imgs, 
-                               v=Vector(), ul=(centerx - 150, 490))
+                               v=Vector(), ul=(centerx - 180, 500))
         self.ufo = Alien(game=game, image_list=LandingPage.ufo_imgs, 
-                               v=Vector(), ul=(centerx - 120, 565))
+                               v=Vector(), ul=(centerx - 180, 550))
 
     def get_text(self, font, msg, color): return font.render(msg, True, color, BLACK)
 
@@ -89,7 +92,9 @@ class LandingPage:
             self.screen.blit(self.texts[i], self.rects[i])
 
     def draw(self):
-        self.screen.fill(BLACK)
+        #self.screen.fill(BLACK)
+
+        self.screen.blit(self.bg_img, self.bg_img.get_rect())
         self.alien_one.draw()
         self.alien_two.draw()
         self.alien_three.draw()
